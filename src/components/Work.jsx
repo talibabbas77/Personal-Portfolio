096df0563f15a126eafc9af106c0c5eaada0401c
talibabbas77 <talibali303@gmail.com>
2024-11-03 23:56:56 +0500

    changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,10 +4,11 @@ import proj2 from "../Assets/proj2.jpg";
 import proj3 from "../Assets/proj3.jpg";
 import proj4 from "../Assets/proj4.jpg";
 import proj5 from "../Assets/proj5.jpg";
-import proj6 from "../Assets/proj6.jpg";
-import proj7 from "../Assets/proj7.jpg";
-import proj8 from "../Assets/gemini.png";
+// import proj6 from "../Assets/proj6.jpg";
+import proj7 from "../Assets/proj7.png";
+import proj8 from "../Assets/proj8.png";
 import proj9 from "../Assets/proj9.png";
+import proj10 from "../Assets/gemini.png";
 
 function Work() {
   return (
@@ -17,6 +18,58 @@ function Work() {
         <p className="text-gray-400">Check out some of my recent work.</p>
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={proj8} alt="Project" />
+          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider"></span>
+            <div className="pt-8 text-center">
+              <a href="https://alfatahzarihouse.vercel.app/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={proj7} alt="Project" />
+          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider"></span>
+            <div className="pt-8 text-center">
+              <a href="https://github.com/talibabbas77/YouTube-Clone.git">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={proj10} alt="Project" />
+          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider"></span>
+            <div className="pt-8 text-center">
+              <a href="https://github.com/talibabbas77/Gemini-Clone.git">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={proj9} alt="Project" />
+          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider"></span>
+            <div className="pt-8 text-center">
+              <a href="https://github.com/talibabbas77/devHub_reactjs.git">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
           <img src={proj1} alt="Project" />
           <div className="opacity-0 group hover:opacity-100 bg-[black]/30 absolute inset-0 flex flex-col justify-center items-center">
@@ -75,58 +128,6 @@ function Work() {
             <span className="text-2xl font-bold text-white tracking-wider"></span>
             <div className="pt-8 text-center">
               <a href="/">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-          <img src={proj6} alt="Project" />
-          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider"></span>
-            <div className="pt-8 text-center">
-              <a href="/">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-          <img src={proj7} alt="Project" />
-          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider"></span>
-            <div className="pt-8 text-center">
-              <a href="https://github.com/talibabbas77/YouTube-Clone.git">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-          <img src={proj8} alt="Project" />
-          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider"></span>
-            <div className="pt-8 text-center">
-              <a href="https://github.com/talibabbas77/Gemini-Clone.git">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-          <img src={proj9} alt="Project" />
-          <div className="opacity-0 group hover:opacity-100 bg-[black]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider"></span>
-            <div className="pt-8 text-center">
-              <a href="https://github.com/talibabbas77/login_register_form-react.git">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg">
                   Live
                 </button>
